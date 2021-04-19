32998a76a36acab36fb6256cd2cf13f90ba8f252
Bharuthy <51150382+Bharuthy@users.noreply.github.com>
2021-04-19 10:23:22 -0400

    commit

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormArray, FormGroup} from '@angular/forms';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-signin',
@@ -10,11 +11,18 @@ import { Router } from '@angular/router';
 export class SigninComponent implements OnInit {
   userForm:FormGroup;
   hide:boolean = true;
-  constructor(private router:Router) {
+  data:any = [];
+  constructor(private router:Router, private http: HttpClient) {
     this.userForm = new FormGroup({
       'username': new FormControl(),
       'password': new FormControl()
     });
+    this.http.get('http://localhost/phpmyadmin/tbl_sql.php?db=mysql&table=customer').subscribe(data => {
+    this.data.push(data);
+    console.log(this.data);
+   
+    
+    }, error => console.error(error));
    }
   ngOnInit(): void {
   }
